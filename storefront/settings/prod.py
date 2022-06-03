3e11083e9e47e6874337e535246d233ee1e069a9
Arunsingh <arunsingh19101999@gmail.com>
2022-06-03 15:44:01 +0530

    modify procfile

--- storefront/settings/prod.py
+++ storefront/settings/prod.py
@@ -7,7 +7,7 @@ DEBUG = False
 
 SECRET_KEY = os.environ['SECRET_KEY']
 
-ALLOWED_HOSTS = ['arunsingh-pro.herokuapp.com']
+ALLOWED_HOSTS = ['arunsingh-prod.herokuapp.com']
 
 DATABASES = {
     'default' : dj_database_url.config()
