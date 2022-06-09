2b154f382b1bf2927bb82d5c0b76dca505e26542
Arunsingh G <103097301+Arunsingh19@users.noreply.github.com>
2022-06-09 16:27:02 +0530

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,6 @@ from django.views.generic import TemplateView
 from django.urls import path
 from . import views
 
-urlpatterns = [
-    path('', TemplateView.as_view(template_name='core/index.html'))
-]
+# urlpatterns = [
+#     path('', TemplateView.as_view(template_name='core/index.html'))
+# ]
