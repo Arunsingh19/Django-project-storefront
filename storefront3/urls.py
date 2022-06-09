5b94f6ba5ab556f85a4759120c719d087ba37a91
Arunsingh G <103097301+Arunsingh19@users.noreply.github.com>
2022-06-09 16:23:18 +0530

    Update urls.py

--- storefront3/urls.py
+++ storefront3/urls.py
@@ -21,10 +21,10 @@ admin.site.site_header = 'Storefront Admin'
 admin.site.index_title = 'Admin'
 
 urlpatterns = [
-    path('', include('core.urls')),
+    path('', include('store.urls')),
     path('admin/', admin.site.urls),
     path('playground/', include('playground.urls')),
-    path('store/', include('store.urls')),
+#     path('store/', include('store.urls')),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
     path('__debug__/', include(debug_toolbar.urls)),
